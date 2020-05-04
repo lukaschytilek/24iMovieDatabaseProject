9f333919a8e32c29ad2d37cbd88d49fbfbc868c0
Lukas Chytilek <chytilek@elkoep.cz>
2020-05-04 08:18:02 +0200

    init

---
+++ MovieDatabase24i/MovieDatabase24i/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDatabase24i
+//
+//  Created by chytilek on 04/05/2020.
+//  Copyright © 2020 lukaschytilek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
